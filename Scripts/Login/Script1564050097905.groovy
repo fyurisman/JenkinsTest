ef280136ef0aad1e9f80837a81d59a37ea3460d2
Felix Y <Felix Y@LAPTOP-FKLO5HNS>
2019-07-26 10:58:28 +0700

    commit

---
+++ Scripts/Login/Script1564050097905.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.setViewPortSize(1024, 768)
+
+WebUI.getViewportHeight()
+
+WebUI.getViewportWidth()
+
+WebUI.navigateToUrl('https://dev.sehatq.com/')
+
+WebUI.waitForPageLoad(5)
+
+WebUI.click(findTestObject('Object Repository/New Folder/button_Login  Daftar Baru'))
+
+WebUI.setText(findTestObject('Object Repository/New Folder/input_Alamat E-Mail_email'), 'kuning@color.com')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/New Folder/input_Kata Sandi_password'), 'VU1orW/zbPOwxjV+uFYKbg==')
+
+WebUI.click(findTestObject('Object Repository/New Folder/button_Masuk'))
+
+WebUI.delay(1)
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>72cae063-5a76-42d2-b436-519d774fc196</testSuiteGuid>
+   <testCaseLink>
+      <guid>1ba78fea-79cb-408a-9381-1ce6f2e3fa89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
